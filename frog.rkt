37310aaaf775f8395942509bb4509e84774e549f
Greg Hendershott <greghendershott@gmail.com>
2013-03-08 12:04:17 -0500

    Refactor and get older/newer working for post pages.
    Do one read to get a master posts list.
    
    Sort that list by date, newest first.
    
    Then write post pages from that list as a second step. That way, each
    post page can be given its older and newer siblings.
    
    Furthermore, we can do the index pages from that master list.
    
    The index page for all posts is more-clearly a special case of the
    index pages for specific tags.
    
    Still to-do: Paginating the index pages -- so no more than say 10 post
    summaries per page -- with older/newer links to the other pages for
    that index.

--- frog.rkt
+++ frog.rkt
@@ -20,94 +20,75 @@
 
 (define all-tags (make-hash)) ;; (hashof string? exact-positive-integer?)
 
-(struct index (title uri date tags blurb more?))
+(struct post (title dest-path uri date tags blurb more? body))
 
 ;; A function for fold-files
-(define (write-post path type v)
+(define (read-post path type v)
   (cond
     [(eq? type 'file)
      (define-values (base name must-be-dir?) (split-path path))
      (match (path->string name)
-       [(pregexp "^\\d{4}-\\d{2}-\\d{2}-.+?\\.(?:md|markdown)$")
-        (define dest-path (~> (build-path (www-path) name)
-                              (path-replace-suffix ".html")))
+       [(pregexp "^\\d{4}-\\d{2}-\\d{2}-\\d{2}-\\d{2}-\\d{2}-.+?\\.(?:md|markdown)$")
         (define xs (with-input-from-file path read-markdown))
         ;; Split to the meta-data and the body
         (define-values (title date tags body) (meta-data xs))
-        ;; Add these tags to the set
-        (for ([x tags])
-          (unless (equal? x "")
-            (hash-set! all-tags x (add1 (hash-ref all-tags x 0)))))
-        ;; Split out the blurb (may be less than the entire body)
-        (define-values (blurb more?) (above-the-fold body))
-        ;; Make up a body for the post
-        (define post-body (post-xexpr title date tags body))
-        (~> post-body
-            (bodies->page title)
-            xexpr->string
-            (display-to-file dest-path #:exists 'replace))
-        (eprintf (str "Generated ~a\n"
-                      "     from ~a\n") dest-path path)
-        (cons (index title
-                     (abs->rel dest-path)
-                     date
-                     tags
-                     blurb
-                     more?)
-              v)]
+        (cond [(member "DRAFT" tags)
+               (eprintf (str "Skipping ~a\n"
+                             "         because it has the tag, 'DRAFT'\n")
+                        (abs->rel/top path))
+               v]
+              [else
+               (eprintf "Reading ~a\n" (abs->rel/top path))
+               ;; Add these tags to the set
+               (for ([x tags])
+                 (unless (equal? x "")
+                   (hash-set! all-tags x (add1 (hash-ref all-tags x 0)))))
+               ;; Split out the blurb (may be less than the entire body)
+               (define-values (blurb more?) (above-the-fold body))
+               (define dest-path (~> (build-path (www-path) name)
+                                     (path-replace-suffix ".html")))
+               (cons (post title
+                           dest-path
+                           (abs->rel/www dest-path)
+                           date
+                           tags
+                           blurb
+                           more?
+                           body)
+                     v)])]
        [else
-        (eprintf "Skipping ~s; not named YYYY-MM-DD-title.{md,markdown}\n"
-                 path)
+        (eprintf
+         (str "Skipping ~a\n"
+              "         Not named YYYY-MM-DD-HH-MM-SS-TITLE.{md,markdown}\n")
+         (abs->rel/top path))
         v])]
     [else v]))
 
-;; A function for fold-files
-(define (do-feed-item path type v)
-  (cond
-    [(eq? type 'file)
-     (define-values (base name must-be-dir?) (split-path path))
-     (match (path->string name)
-       [(pregexp "^\\d{4}-\\d{2}-\\d{2}-.+?\\.(?:md|markdown)$")
-        (define xs (with-input-from-file path read-markdown))
-        ;; Split to the meta-data and the body
-        (define-values (title date tags body) (meta-data xs))
-        ;; Add these tags to the set
-        (for ([x tags])
-          (unless (equal? x "")
-            (hash-set! all-tags x (add1 (hash-ref all-tags x 0)))))
-        ;; Split out the blurb (may be less than the entire body)
-        (define-values (blurb more?) (above-the-fold body))
+(define (write-post-page p older newer)
+  (match-define (post title dest-path uri date tags blurb more? body) p)
+  (eprintf "Generating ~a\n" (abs->rel/top dest-path))
+  (~> (post-xexpr title date tags body older newer)
+      (bodies->page #:title title)
+      xexpr->string
+      (display-to-file dest-path #:exists 'replace)))
 
-        (define uri (path->string path)) ;; TODO: real URI, with hostname
-        (cons
-         `(entry
-           ()
-           (title ([type "text"]) ,title)
-           (link ([rel "alternate"]
-                  [href ,uri]))
-           (id () ,title) ;; TODO: some other ID??
-           (published () ,date)
-           (updated () ,date)
-           (content
-            ([type "html"])
-            ,(xexpr->string
-              `(html
-                ,@blurb
-                ,@(cond [more? `((a ([href ,uri])
-                                    "Continue reading ..."))]
-                        [else '()])))))
-         v)]
-       [else
-        (eprintf "Skipping ~s; not named YYYY-MM-DD-title.{md,markdown}\n"
-                 path)
-        v])]
-    [else v]))
+(define (post-xexpr title date tags body older newer)
+  `((h1 ,title)
+    (p ,date)
+    ,(tags->xexpr tags)
+    ,@(filter (negate more?) body)
+    ,(older/newer-nav older newer)))
 
-(define (post-xexpr title date tags body)
-  (list* `(h1 ,title)
-         `(p ,date)
-          (tags->xexpr tags)
-          (filter (negate more?) body)))
+(define (older/newer-nav older newer)
+  `(p ,(cond [newer
+              `(a ([href ,(post-uri newer)])
+                  'larr "Newer:" ,(post-title newer))]
+             [else ""])
+      'nbsp 'nbsp
+      ,(cond [older
+              `(a ([href ,(post-uri older)])
+                  "Older:" ,(post-title older) 'rarr)]
+             [else ""])))
          
 (define (meta-data xs)
   (match (first xs)
@@ -139,11 +120,17 @@
     [(list p "<!-- more -->") #t]
     [else #f]))
 
-(define (abs->rel path)
+(define (abs->rel/www path)
   (let ([path (path->string path)]
         [root (path->string (www-path))])
     (match path
       [(pregexp (str "^" (regexp-quote root) "(.+$)") (list _ x)) x])))
+
+(define (abs->rel/top path)
+  (let ([path (path->string path)]
+        [root (path->string (top))])
+    (match path
+      [(pregexp (str "^" (regexp-quote root) "/(.+$)") (list _ x)) x])))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -152,7 +139,11 @@
 (define-runtime-path
   google-analytics-template.js
   "google-analytics-template.js")
-(define (bodies->page xs title)
+(define (bodies->page xs
+                      #:title title
+                      #:feed [feed "all"]
+                      #:newer [newer #f]
+                      #:older [older #f])
   (define (toc-xexpr)
     (match (toc xs)
       [`(div ([class "toc"]) (ol ,contents ...))
@@ -165,7 +156,7 @@
     (define alist (~> (for/list ([(k v) (in-hash all-tags)])
                         (cons k v))
                       (sort string-ci<=? #:key car)))
-    `(p "Posts tagged:"
+    `(p "Tags:"
         (ul ,@(for/list ([(k v) (in-dict alist)])
                 `(li ,(tag->xexpr k (format " (~a)" v)))))))
   `(html
@@ -177,7 +168,7 @@
      (meta ([name "author"][content ,(current-author)]))
      (link ([href "/css/bootstrap.css"][rel "stylesheet"][type "text/css"]))
      (link ([href "/css/custom.css"][rel "stylesheet"][type "text/css"]))
-     (link ([href "/atom.xml"]
+     (link ([href ,(str "/feeds/" feed ".xml")]
             [type "application/atom+xml"]
             [rel "alternate"]
             [title ,(current-title)]))
@@ -231,20 +222,23 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (write-index xs title file) ;; (listof index?) -> any
+(define (write-index xs title feed file) ;; (listof post?) -> any
   (define (string-prepend s prepend)
     (string-append prepend s))
-  (~> (for/list ([x (in-list xs)])
-        (match-define (index title uri date tags blurb more?) x)
+  (~> (cons
+       `(h1 ,title)
+       (for/list ([x (in-list xs)])
+        (match-define (post title dest-path uri date tags blurb more? body) x)
         `(div ([class "index-post"])
               (h1 (a ([href ,uri]) ,title))
               (p ,date)
               ,(tags->xexpr tags)
               ,@blurb
-              ,@(cond [more? `((a ([href ,uri]) (em "Continue reading ...")))]
-                      [else '()])))
+              ,@(cond [more? `((a ([href ,uri])
+                                  (em "Continue reading ...")))]
+                      [else '()]))))
       (add-between `(hr))
-      (bodies->page title)
+      (bodies->page #:title title #:feed feed)
       xexpr->string
       (string-prepend "<!DOCTYPE html>\n")
       (display-to-file file #:exists 'replace)))
@@ -263,23 +257,41 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (write-atom-feed)
+(define (write-atom-feed xs title file)
   (define x
     `(feed
       ([xmlns "http://www.w3.org/2005/Atom"]
        [xml:lang "en"])
-      (title ([type "text"]) ,(current-title))
+      (title ([type "text"]) ,(str (current-title) ": " title))
       ;; (link ([href ,(gplus-self-uri g)]
       ;;        [rel "self"]))
       ;; (link ([href ,(gplus-self-uri g)]))
       ;; (id () ,(gplus-id g))
       ;; (etag () ,(gplus-etag g))
       ;; (updated () ,(gplus-updated g))
-      ,@(fold-files do-feed-item '() (src/posts-path) #f)))
+      ,@(map post->feed-entry-xexpr xs)))
   (~> x
       xexpr->string
-      (display-to-file (build-path (www-path) "atom.xml")
-                       #:exists 'replace)))
+      (display-to-file file #:exists 'replace)))
+
+(define (post->feed-entry-xexpr x) ;; post? -> xexpr?
+  (match-define (post title dest-path uri date tags blurb more? body) x)
+  `(entry
+    ()
+    (title ([type "text"]) ,title)
+    (link ([rel "alternate"]
+           [href ,uri]))
+    (id () ,title) ;; TODO: some other ID??
+    (published () ,date)
+    (updated () ,date)
+    (content
+     ([type "html"])
+     ,(xexpr->string
+       `(html
+         ,@blurb
+         ,@(cond [more? `((a ([href ,uri])
+                             "Continue reading ..."))]
+                 [else '()]))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -296,18 +308,11 @@ EOF
 )
 
 (define (new-post title)
-  (define (two-digits d)
-    (cond [(< d 10) (str "0" (~a d))]
-          [else (~a d)]))
-  (define d (current-date))
-  (define date-str (str #:sep "-"
-                        (~> d date-year two-digits)
-                        (~> d date-month two-digits)
-                        (~> d date-day two-digits)))
-  (define filename (str date-str
-                        "-"
-                        (~> title
-                            string-downcase
+  (define date-str (parameterize ([date-display-format 'iso-8601])
+                     (date->string (current-date) #t)))
+  (define filename (str (~> (str (regexp-replace "T" date-str "-")
+                                 "-"
+                                 (~> title string-downcase))
                             our-encode)
                         ".md"))
   (define pathname (build-path (src/posts-path) filename))
@@ -329,7 +334,7 @@ EOF
   (define (maybe-delete path type v)
     (define (rm p)
       (delete-file p)
-      (eprintf "Deleted ~a\n" p))
+      (eprintf "Deleted ~a\n" (abs->rel/top p)))
     (cond
      [(eq? type 'file)
       (define-values (base name must-be-dir?) (split-path path))
@@ -338,7 +343,8 @@ EOF
             [(equal? (build-path base) (build-path (www-path) "tags/"))
              (rm path)]
             [else (match (path->string name)
-                    [(pregexp "^\\d{4}-\\d{2}-\\d{2}-.+?\\.html$")
+                    [(pregexp
+                      "^\\d{4}-\\d{2}-\\d{2}t\\d{2}-\\d{2}-\\d{2}-.+?\\.html$")
                      (rm path)]
                     [else (void)])])]))
   (fold-files maybe-delete '() (www-path) #f))
@@ -347,22 +353,41 @@ EOF
 
 (define (build)
   (define (post<=? a b)
-    (string<=? (index-date a) (index-date b)))
+    (string<=? (post-date a) (post-date b)))
 
-  ;; Write the posts and get (listof index?) with which to do more work.
-  (define xs (~> (fold-files write-post '() (src/posts-path) #f)
+  ;; Read the posts and get (listof post?) with which to do more work.
+  ;; Sort the list by date, newest first.
+  (define xs (~> (fold-files read-post '() (src/posts-path) #f)
                  (sort (negate post<=?))))
-  ;; Write the index page for each tag
+  ;; Make lists for older and newer items. Just shift each list and
+  ;; add #f to one end.
+  (define older (append (drop xs 1) (list #f)))
+  (define newer (cons #f (take xs (sub1 (length xs)))))
+  (for ([x newer])
+    (displayln (if x (post-title x) "none")))
+  ;; Write the post page
+  (for-each write-post-page
+            xs
+            older
+            newer)
+  ;; For each tag, write the index page and Atom feed
   (for ([(tag _) (in-hash all-tags)])
-    (write-index (filter (lambda (x)
-                           (member tag (index-tags x)))
-                         xs)
-                 tag
-                 (build-path (www-path) "tags" (str (our-encode tag) ".html"))))
+    (define xs-this-tag (filter (lambda (x)
+                                  (member tag (post-tags x)))
+                                xs))
+    (write-index
+     xs-this-tag
+     (str "Posts tagged '" tag "'")
+     (our-encode tag)
+     (build-path (www-path) "tags" (str (our-encode tag) ".html")))
+    (write-atom-feed
+     xs-this-tag
+     (str "Posts tagged '" tag "'")
+     (build-path (www-path) "feeds" (str (our-encode tag) ".xml"))))
   ;; Write the index for all
-  (write-index xs "All Tags" (build-path (www-path) "index.html"))
-  ;; Write Atom feed file
-  (write-atom-feed))
+  (write-index xs "All Posts" "all" (build-path (www-path) "index.html"))
+  ;; Write Atom feed for all
+  (write-atom-feed xs "All Posts" (build-path (www-path) "feeds" "all.xml")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -370,7 +395,7 @@ EOF
          net/sendurl)
 
 (define (preview)
-  (define port 3000)
+  (define port 3001)
   (printf "Start preview web server at localhost:~a\n" port)
   (define stop
     (parameterize ([serve:home (www-path)]
@@ -399,9 +424,14 @@ EOF
 
 ;;; Can uncomment these during development -- but don't commit that way!
 
-;; (clean)
-;; (build)
-;; (preview)
+#|
+
+(begin
+  (clean)
+  (build)
+  (preview))
+
+|#
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -433,13 +463,17 @@ EOF
 
 TODO:
 
-- Index pages: Sort posts in reverse date order.
-
 - Index pages: Paingate, limiting to N posts per page, with
   Older/Newer nav.
 
 - Post pages: Add Older/Newer nav.
 
+DONE:
+
+- Index pages: Sort posts in reverse date order.
+
 - DRAFT tag to exclude from generation.
+
+- Feeds for every tag.
 
 |#
