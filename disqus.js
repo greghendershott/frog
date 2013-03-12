f30d17797f9c638781556ab7aa32018318960644
Greg Hendershott <greghendershott@gmail.com>
2013-03-12 11:26:18 -0400

    Add disqus.js. Should have been in commit 3df2a3a9.

---
+++ disqus.js
@@ -0,0 +1,10 @@
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+var disqus_shortname = '~a'; // required: replace example with your
+                             // forum shortname
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
