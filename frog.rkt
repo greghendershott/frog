dc392194fa3b8ad71ea05349beed659bcc974c82
Greg Hendershott <greghendershott@gmail.com>
2013-03-18 14:50:21 -0400

    Add tests for our-encode

--- frog.rkt
+++ frog.rkt
@@ -492,13 +492,19 @@
                       (cond [(or (char-alphabetic? c)
                                  (char-numeric? c)) c]
                             [else #\-])))
-      (re* #px"-{2,}" "-") ;only one hyphen in a row
-      (re #px"-$" "")))    ;no hyphen at end
+      (re* #px"-{2,}" "-")              ;only one hyphen in a row
+      (re #px"-{1,}$" "")))             ;no hyphen at end
 
 (define (re* s rx new)
   (regexp-replace* rx s new))
 (define (re s rx new)
   (regexp-replace rx s new))
+
+(module+ test
+  (check-equal? (our-encode "Foo? Bar. Baz.")
+                "Foo-Bar-Baz")
+  (check-equal? (our-encode "Here's a question--how many hyphens???")
+                "Here-s-a-question-how-many-hyphens"))
 
 ;; Less typing, but also returns its value so good for sticking in ~>
 ;; for debugging
@@ -506,6 +512,7 @@
   (pretty-print v)
   v)
 
+;; Like display-to-file, but makes directories if needed.
 (define (display-to-file* v path #:exists exists #:mode [mode 'binary])
   (make-directories-if-needed path)
   (display-to-file v path #:exists exists #:mode mode))
