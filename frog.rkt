6ca0b2fef615c4e6341ba26f30c9a85019d2c54f
Manu <manu.delab@gmail.com>
2013-04-03 23:44:00 +1300

    bootstrap.js errored on jQuery < 1.7

--- frog.rkt
+++ frog.rkt
@@ -335,7 +335,7 @@
                       [rel "alternate"]
                       [title ,(str (current-title) ": " feed)]))
                ;; JS
-               ,(script/js "http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js")
+               ,(script/js "http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js")
                ,(bootstrap-js)
                ,@(google-analytics))
          (body
