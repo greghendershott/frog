50983af4bca092fd6ac47d9d4c686cad033efd97
Greg Hendershott <greghendershott@gmail.com>
2013-06-03 13:03:13 -0400

    Define two functions instead of using case-lambda.

--- frog/pygments.rkt
+++ frog/pygments.rkt
@@ -21,52 +21,48 @@
   (sleep 1)
   (define (running?)
     (eq? 'running (pyg-proc 'status)))
-  (case-lambda
-    [(code lang) ;; lex :: string? string? -> (listof xexpr?)
-     (cond [(running?)
-            (file-stream-buffer-mode pyg-out 'line)
-            (displayln lang pyg-out)
-            (displayln code pyg-out)
-            (displayln "__END__" pyg-out)
-            (let loop ([s ""])
-              (match (read-line pyg-in 'any)
-                ["__END__" (~> (open-input-string s)
-                               (h:read-html-as-xml)
-                               ((lambda (xs) (make-element #f #f '*root '() xs)))
-                               xml->xexpr
-                               cddr)]
-                [(? string? v) (loop (str s v "\n"))]
-                [_ (copy-port pyg-err (current-output-port)) ;echo error msg
-                   `((pre ,code))]))]
-           [else `((pre ,code))])]
-    [() ;; stop :: -> number?
-     (cond [(running?)
-            (file-stream-buffer-mode pyg-out 'line)
-            (displayln "__EXIT__" pyg-out)
-            (begin0 (or (pyg-proc 'exit-code) (pyg-proc 'kill))
-              (close-input-port pyg-in)
-              (close-output-port pyg-out)
-              (close-input-port pyg-err))]
-           [else 0])]))
+  (unless (running?)
+    (prn0 "Pygments not installed. Using plain `pre` blocks."))
+  (define (lex code lang) ;; string? string? -> (listof xexpr?)
+    (cond [(running?)
+           (file-stream-buffer-mode pyg-out 'line)
+           (displayln lang pyg-out)
+           (displayln code pyg-out)
+           (displayln "__END__" pyg-out)
+           (let loop ([s ""])
+             (match (read-line pyg-in 'any)
+               ["__END__" (~> (open-input-string s)
+                              (h:read-html-as-xml)
+                              ((lambda (xs) (make-element #f #f '*root '() xs)))
+                              xml->xexpr
+                              cddr)]
+               [(? string? v) (loop (str s v "\n"))]
+               [_ (copy-port pyg-err (current-output-port)) ;echo error msg
+                  `((pre ,code))]))]
+          [else `((pre ,code))]))
+  (define (stop) ;; -> number?
+    (cond [(running?)
+           (file-stream-buffer-mode pyg-out 'line)
+           (displayln "__EXIT__" pyg-out)
+           (begin0 (or (pyg-proc 'exit-code) (pyg-proc 'kill))
+             (close-input-port pyg-in)
+             (close-output-port pyg-out)
+             (close-input-port pyg-err))]
+          [else 0]))
+  (values lex stop))
 
-(define pyg #f)
+(define pyg-lex #f)
+(define pyg-stop #f)
 
 (define (start-pygments)
   (prn2 "Trying to start pygments process")
-  (set! pyg (start-pygments-process)))
+  (set!-values (pyg-lex pyg-stop) (start-pygments-process)))
 
 (define (stop-pygments)
   (prn2 "Stopping pygments process")
-  (pyg)
-  (set! pyg #f))
+  (pyg-stop)
+  (set!-values (pyg-lex pyg-stop) (values #f #f)))
 
 (define (pygmentize code lang) ;; string? string? -> (listof xexpr?)
-  (cond [pyg (pyg code lang)]
+  (cond [pyg-lex (pyg-lex code lang)]
         [else `((pre ,code))]))
-
-;; (start-pygments)
-;; (define-runtime-path pygments.rkt "pygments.rkt")
-;; (pretty-print (pygmentize (file->string pygments.rkt) "racket"))
-;; (stop-pygments)
-;; ;; How does it handle this?
-;; (pretty-print (pygmentize (file->string pygments.rkt) "racket"))
