529acb6c873e6ef469d484c352c70971ba3fcad0
Greg Hendershott <greghendershott@gmail.com>
2013-06-11 14:35:36 -0400

    Improve xexprs->description and xexpr->markdown.
    - Allow suffix after more block elements, not just p.
    - Put code blocks in backticks.
    - Simplify match patterns.

--- frog/xexpr2text.rkt
+++ frog/xexpr2text.rkt
@@ -9,8 +9,9 @@
   (require rackunit))
 
 (define (xexprs->description xs [num 3])
-  (str (string-join (map xexpr->markdown (take<= xs num)))
-       " ..."))
+  (str (string-join (map (curryr xexpr->markdown " ") (take<= xs num))
+                    "")
+       "..."))
 
 (module+ test
   (check-equal?
@@ -28,22 +29,28 @@
                           (p "A " (em "paragraph") " of some stuff.")
                           (p "A " (em "paragraph") " of some stuff."))
                         3)
-   "A heading: A _paragraph_ of some stuff.  ..."))
+   "A heading: A _paragraph_ of some stuff. ..."))
 
 ;; Not full markdown, just a "lite" variation.
-(define (xexpr->markdown x [p-suffix ""])
+(define (xexpr->markdown x [block-suffix ""])
   (define (heading? s)
     (memq s '(h1 h2 h3 h4 h5 h6 h7 h8 h9)))
-  (define (recur x)
-    (xexpr->markdown x p-suffix))
+  (define (block? s)
+    (memq s '(p div li)))
   (define (->s es)
-    (apply str (map recur es)))
-  (match x
-    [`(em            ([,_ ,_] ...) ... ,es ...) (str "_" (->s es) "_")]
-    [`(strong        ([,_ ,_] ...) ... ,es ...) (str "**" (->s es) "**")]
-    [`(,(? heading?) ([,_ ,_] ...) ... ,es ...) (str (->s es) ":")]
-    [`(p             ([,_ ,_] ...) ... ,es ...) (str (->s es) p-suffix)]
-    [`(,(? symbol?)  ([,_ ,_] ...) ... ,es ...) (str (->s es))]
+    (apply str (map (curryr xexpr->markdown block-suffix) es)))
+  (define (normalize x) ;; ensure xexpr has explicit attributes
+    (match x
+      [`(,(? symbol? tag) ([,(? symbol? ks) ,(? string? vs)] ...) ,es ...) x]
+      [`(,(? symbol? tag) ,es ...) `(,tag () ,@es ...)] ;add '() attribs
+      [_ x]))
+  (match (normalize x)
+    [`(em            ,_ ,es ...) (str "_" (->s es) "_")]
+    [`(strong        ,_ ,es ...) (str "**" (->s es) "**")]
+    [`(code          ,_ ,es ...) (str "`" (->s es) "`")]
+    [`(,(? heading?) ,_ ,es ...) (str (->s es) ": ")]
+    [`(,(? block?)   ,_ ,es ...) (str (->s es) block-suffix)]
+    [`(,(? symbol?)  ,_ ,es ...) (str (->s es))]
     [(? string? s) s]
     ['ndash "--"]
     ['mdash "--"]
@@ -72,6 +79,6 @@
   (check-equal? (xexpr->markdown '(p "M" 'amp "Ms" 'mdash "gotta love 'em"))
                 "M&Ms--gotta love 'em")
   (check-equal? (xexpr->markdown '(div (p "Hi.") (p "Hi.")) "\n")
-                "Hi.\nHi.\n")
+                "Hi.\nHi.\n\n")
   (check-equal? (xexpr->markdown '(p "Hi" #x20 "there"))
                 "Hi there"))
