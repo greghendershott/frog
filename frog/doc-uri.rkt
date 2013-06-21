1a0d45af0034582a6e872a43cf649e64f8fa77f6
Greg Hendershott <greghendershott@gmail.com>
2013-06-21 12:21:58 -0400

    Refactor into cleaner set of functions.
    Also optimize: Build and use a hasheq cache from name to libs (instead
    of repeatedly scanning the 27,000+ items in the entry? list, as in the
    original code I'd cribbed from help/help-utils).

--- frog/doc-uri.rkt
+++ frog/doc-uri.rkt
@@ -1,6 +1,8 @@
-#lang racket/base
+#lang racket
 
-(provide doc-uri)
+(provide doc-uri
+         sym-mods
+         doc-uri/sym-in-mod)
 
 (require racket/contract
          racket/match
@@ -12,67 +14,73 @@
 (module+ test
   (require rackunit))
 
-(define xref (load-collections-xref))
-
-;; Starting point: help/help-utils `search-for-exports`.
-(define/contract (doc-uri sym)
-  (symbol? . -> . (or/c string?
-                        'none
-                        (listof (cons/c symbol? string?))))
-  (define (not-planet x)
-    (match x
-      [`(planet ,_ ...) #f]
-      [_ #t]))
-  (define mods
-    (filter
-     not-planet
-     (for/list ([entry (in-list (xref-index xref))]
-                #:when
-                (and (exported-index-desc? (entry-desc entry))
-                     (eq? sym (exported-index-desc-name (entry-desc entry)))))
-       (car (exported-index-desc-from-libs (entry-desc entry))))))
-  ;; If provided by 0 or 1 module, that's simple.
-  ;; If multi mods, and one is racket/base or racket, use that.
-  ;; Else return them all, sorted by module name.
-  (match mods
-    [(list) 'none]
-    [(list mod) (doc-uri/mod sym mod)]
-    [(list-no-order (or 'racket/base) _ ...) (doc-uri/mod sym 'racket/base)]
-    [(list-no-order (or 'racket) _ ...) (doc-uri/mod sym 'racket)]
-    [_ (sort (map cons
-                  mods
-                  (map (curry doc-uri/mod sym) mods))
-             symbol<? #:key car)]))
-
-(define (doc-uri/mod id mod)
-  ;; (symbol? symbol? . -> . string?)
-  ;; throws exn if mod doesn't exist
-  (define tag (xref-binding->definition-tag xref (list mod id) #f))
-  (cond [tag (define-values (path anchor)
-               (xref-tag->path+anchor
-                xref
-                tag
-                #:external-root-url "http://docs.racket-lang.org/"))
-             (string-append path "#" anchor)]
-        [else ""]))
-
-(define (symbol<? a b)
-  (string<? (symbol->string a) (symbol->string b)))
+;; This simplified interface returns one doc URI or #f for a given
+;; symbol. When a symbol is provided by multiple libs: If one is
+;; racket or racket/base, then that doc is used, else #f.
+(define/contract (doc-uri sym #:root-uri [root "http://docs.racket-lang.org/"])
+  ((symbol?) (#:root-uri (or/c #f string?)) . ->* . (or/c string? #f))
+  (define mod
+    (match (sym-mods sym)
+      [(list) #f]
+      [(list m) m]
+      [(list-no-order (and (or 'racket/base 'racket) m) _ ...) m]
+      [_ #f]))
+  (and mod (doc-uri/sym-in-mod sym mod root)))
 
 (module+ test
   (define get-pure-port-uri
     "http://docs.racket-lang.org/net/url.html#(def._((lib._net/url..rkt)._get-pure-port))")
   (check-equal? (doc-uri 'get-pure-port) get-pure-port-uri)
   (check-equal? (doc-uri (string->symbol "get-pure-port")) get-pure-port-uri)
-  (check-equal? (doc-uri '|NOT DEFINED|) 'none)
+  (check-equal? (doc-uri '|NOT DEFINED|) #f)
   (check-equal? (doc-uri 'rectangle)
-                '((2htdp/image . "http://docs.racket-lang.org/teachpack/2htdpimage.html#(def._((lib._2htdp/image..rkt)._rectangle))")
-                  (htdp/image . "http://docs.racket-lang.org/teachpack/image.html#(def._((lib._htdp/image..rkt)._rectangle))")
-                  (slideshow/pict . "http://docs.racket-lang.org/slideshow/Basic_Pict_Constructors.html#(def._((lib._slideshow/pict..rkt)._rectangle))")
-                  (teachpack/deinprogramm/image . "http://docs.racket-lang.org/deinprogramm/image.html#(def._((lib._deinprogramm/image..rkt)._rectangle))"))
+                #f
                 "`rectangle` is in multiple libraries, none racket/base or racket")
   (define printf-uri
     "http://docs.racket-lang.org/reference/Writing.html#(def._((quote._~23~25kernel)._printf))")
   (check-equal? (doc-uri 'printf)
                 printf-uri
                 "`printf` provided by multi libs, but one is racket/base"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define xref (load-collections-xref))
+
+;; (symbol? . -> . (listof symbol?))
+(define sym-mods
+  (let ([cache #f])
+    (lambda (sym)
+      (unless cache
+        (set! cache (make-hasheq))
+        (for ([x (in-list (xref-index xref))])
+          (match x
+            [(entry words content tag (exported-index-desc name from-libs))
+             (hash-set! cache name (append (hash-ref cache name '())
+                                           (filter symbol? from-libs)))]
+            [_ (void)])))
+      (hash-ref cache sym #f))))
+
+(module+ test
+  (define (symbol<? a b)
+    (string<? (symbol->string a) (symbol->string b)))
+  (check-equal?
+   (sort (sym-mods 'printf) symbol<?)
+   '(lang/htdp-advanced lazy plai/gc2/mutator plai/mutator racket/base))
+  (check-equal?
+   (sort (sym-mods 'rectangle) symbol<?)
+   '(2htdp/image htdp/image slideshow/pict teachpack/deinprogramm/image)))
+
+(define/contract (doc-uri/sym-in-mod id mod ext-root-uri)
+  (symbol? symbol? (or/c #f string?) . -> . (or/c #f string?)) ;or exn
+  (define tag (xref-binding->definition-tag xref (list mod id) #f))
+  (and tag
+       (let-values ([(path anchor)
+                     (xref-tag->path+anchor xref
+                                            tag
+                                            #:external-root-url ext-root-uri)])
+         (format "~a#~a" path anchor))))
+
+(module+ test
+  (check-equal?
+   (doc-uri/sym-in-mod 'write 'racket "http://docs.racket-lang.org/")
+   "http://docs.racket-lang.org/reference/Writing.html#(def._((quote._~23~25kernel)._write))"))
