e6a59575a49a09b274b8642c58802029b0629f01
Greg Hendershott <greghendershott@gmail.com>
2013-06-20 23:15:44 -0400

    Various changes:
    - Refactor code.
    
    - In the case where a symbol is provided by multiple modules, doc-uri
      returns a list of modules and URIs (instead of a list of
      modules). That way, a caller could provide a list of the choices for
      a user.
    
    - Ignore Planet modules: They make xref-tag->path+anchor blow up
      because they're not something path-element->string can handle.
    
    - Handle xref-binding->definition-tag returning #f.

--- frog/doc-uri.rkt
+++ frog/doc-uri.rkt
@@ -4,6 +4,7 @@
 
 (require racket/contract
          racket/match
+         racket/function
          setup/xref 
          scribble/xref
          scribble/manual-struct)
@@ -13,53 +14,65 @@
 
 (define xref (load-collections-xref))
 
-(define/contract (doc-uri sym [mod #f])
-  ((symbol?) (symbol?) . ->* . (or/c string?
-                                     'none
-                                     (listof symbol?)))
-  (cond [mod (doc-uri/mod&id mod sym)]
-        [else (doc-uri/id sym)]))
-
 ;; Starting point: help/help-utils `search-for-exports`.
-(define/contract (doc-uri/id sym)
+(define/contract (doc-uri sym)
   (symbol? . -> . (or/c string?
                         'none
-                        (listof symbol?)))
+                        (listof (cons/c symbol? string?))))
+  (define (not-planet x)
+    (match x
+      [`(planet ,_ ...) #f]
+      [_ #t]))
   (define mods
-    (for/list ([entry (in-list (xref-index xref))]
-               #:when
-               (and (exported-index-desc? (entry-desc entry))
-                    (eq? sym (exported-index-desc-name (entry-desc entry)))))
-      (car (exported-index-desc-from-libs (entry-desc entry)))))
+    (filter
+     not-planet
+     (for/list ([entry (in-list (xref-index xref))]
+                #:when
+                (and (exported-index-desc? (entry-desc entry))
+                     (eq? sym (exported-index-desc-name (entry-desc entry)))))
+       (car (exported-index-desc-from-libs (entry-desc entry))))))
   ;; If provided by 0 or 1 module, that's simple.
   ;; If multi mods, and one is racket/base or racket, use that.
-  ;; Else can't do anything, just return the mods FYI for error result.
+  ;; Else return them all, sorted by module name.
   (match mods
     [(list) 'none]
-    [(list mod) (doc-uri/mod&id mod sym)]
-    [(list-no-order (or 'racket/base) _ ...) (doc-uri/mod&id 'racket/base sym)]
-    [(list-no-order (or 'racket) _ ...) (doc-uri/mod&id 'racket sym)]
-    [_ mods]))
+    [(list mod) (doc-uri/mod sym mod)]
+    [(list-no-order (or 'racket/base) _ ...) (doc-uri/mod sym 'racket/base)]
+    [(list-no-order (or 'racket) _ ...) (doc-uri/mod sym 'racket)]
+    [_ (sort (map cons
+                  mods
+                  (map (curry doc-uri/mod sym) mods))
+             symbol<? #:key car)]))
 
-(define/contract (doc-uri/mod&id mod id)
-  (symbol? symbol? . -> . string?) ;throws exn if mod doesn't exist
+(define (doc-uri/mod id mod)
+  ;; (symbol? symbol? . -> . string?)
+  ;; throws exn if mod doesn't exist
   (define tag (xref-binding->definition-tag xref (list mod id) #f))
-  (define-values (path anchor)
-    (xref-tag->path+anchor xref
-                           tag
-                           #:external-root-url "http://docs.racket-lang.org/"))
-  (string-append path "#" anchor))
+  (cond [tag (define-values (path anchor)
+               (xref-tag->path+anchor
+                xref
+                tag
+                #:external-root-url "http://docs.racket-lang.org/"))
+             (string-append path "#" anchor)]
+        [else (displayln id)
+              (displayln mod)
+              ""]))
+
+(define (symbol<? a b)
+  (string<? (symbol->string a) (symbol->string b)))
 
 (module+ test
   (define get-pure-port-uri
     "http://docs.racket-lang.org/net/url.html#(def._((lib._net/url..rkt)._get-pure-port))")
-  (check-equal? (doc-uri 'get-pure-port 'net/url) get-pure-port-uri)
   (check-equal? (doc-uri 'get-pure-port) get-pure-port-uri)
   (check-equal? (doc-uri (string->symbol "get-pure-port")) get-pure-port-uri)
   (check-equal? (doc-uri '|NOT DEFINED|) 'none)
   (check-equal? (doc-uri 'rectangle)
-                '(2htdp/image htdp/image teachpack/deinprogramm/image
-                              slideshow/pict))
+                '((2htdp/image . "http://docs.racket-lang.org/teachpack/2htdpimage.html#(def._((lib._2htdp/image..rkt)._rectangle))")
+                  (htdp/image . "http://docs.racket-lang.org/teachpack/image.html#(def._((lib._htdp/image..rkt)._rectangle))")
+                  (slideshow/pict . "http://docs.racket-lang.org/slideshow/Basic_Pict_Constructors.html#(def._((lib._slideshow/pict..rkt)._rectangle))")
+                  (teachpack/deinprogramm/image . "http://docs.racket-lang.org/deinprogramm/image.html#(def._((lib._deinprogramm/image..rkt)._rectangle))"))
+                "`rectangle` is in multiple libraries, none racket/base or racket")
   (define printf-uri
     "http://docs.racket-lang.org/reference/Writing.html#(def._((quote._~23~25kernel)._printf))")
   (check-equal? (doc-uri 'printf)
