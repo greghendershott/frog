d6de846c35814d1b2fe284aca55cbb2778870390
Greg Hendershott <greghendershott@gmail.com>
2013-06-22 10:21:08 -0400

    Use memoization for `doc-uri`. Also update/simplify unit tests.

--- frog/doc-uri.rkt
+++ frog/doc-uri.rkt
@@ -4,10 +4,7 @@
          sym-mods
          doc-uri/sym-in-mod)
 
-(require racket/contract
-         racket/match
-         racket/function
-         setup/xref 
+(require setup/xref
          scribble/xref
          scribble/manual-struct)
 
@@ -15,39 +12,46 @@
   (require rackunit))
 
 ;; This simplified interface returns one doc URI or #f for a given
-;; symbol. When a symbol is provided by multiple libs: If one is
-;; racket or racket/base, then that doc is used, else #f.
-(define/contract (doc-uri sym #:root-uri [root "http://docs.racket-lang.org/"])
-  ((symbol?) (#:root-uri (or/c #f string?)) . ->* . (or/c string? #f))
-  (define mod
-    (match (sym-mods sym)
-      [(list) #f]
-      [(list m) m]
-      [(list-no-order (and (or 'racket/base 'racket) m) _ ...) m]
-      [_ #f]))
-  (and mod (doc-uri/sym-in-mod sym mod root)))
+;; symbol. When a symbol is provided by multiple libs: If one of the
+;; libs is racket or racket/base then its doc is used, else #f.
+(define doc-uri
+  (let ([memoizes (make-hasheq)])
+    (lambda (sym #:root-uri [root "http://docs.racket-lang.org/"])
+      ;; ((symbol?) (#:root-uri (or/c #f string?)) . ->* . (or/c string? #f))
+      (define (lookup sym root)
+        (define mod
+          (match (sym-mods sym)
+            [(list) #f]
+            [(list m) m]
+            [(list-no-order (and (or 'racket/base 'racket) m) _ ...) m]
+            [_ #f]))
+        (and mod (doc-uri/sym-in-mod sym mod root)))
+      (define k (string->symbol (format "~a,~a" sym root)))
+      (hash-ref memoizes k (lambda ()
+                             (define v (lookup sym root))
+                             (hash-set! memoizes k v)
+                             v)))))
 
 (module+ test
-  (define get-pure-port-uri
-    "http://docs.racket-lang.org/net/url.html#(def._((lib._net/url..rkt)._get-pure-port))")
-  (check-equal? (doc-uri 'get-pure-port) get-pure-port-uri)
-  (check-equal? (doc-uri (string->symbol "get-pure-port")) get-pure-port-uri)
-  (check-equal? (doc-uri '|NOT DEFINED|) #f)
-  (check-equal? (doc-uri 'rectangle)
-                #f
-                "`rectangle` is in multiple libraries, none racket/base or racket")
-  (define printf-uri
-    "http://docs.racket-lang.org/reference/Writing.html#(def._((quote._~23~25kernel)._printf))")
-  (check-equal? (doc-uri 'printf)
-                printf-uri
-                "`printf` provided by multi libs, but one is racket/base"))
+  (check-equal? (doc-uri 'get-pure-port #:root-uri "")
+                "net/url.html#(def._((lib._net/url..rkt)._get-pure-port))")
+  (check-equal? (doc-uri '|NOT DEFINED|)
+                #f)
+  (check-equal?
+   (doc-uri 'rectangle)
+   #f
+   "`rectangle` is in multiple libraries, none racket/base or racket")
+  (check-equal?
+   (doc-uri 'printf #:root-uri "")
+   "reference/Writing.html#(def._((quote._~23~25kernel)._printf))"
+   "`printf` provided by multi libs, but one is racket/base"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define xref (load-collections-xref))
 
-;; (symbol? . -> . (listof symbol?))
 (define sym-mods
+  ;; (symbol? . -> . (listof symbol?))
   (let ([cache #f])
     (lambda (sym)
       (unless cache
@@ -63,12 +67,16 @@
 (module+ test
   (define (symbol<? a b)
     (string<? (symbol->string a) (symbol->string b)))
-  (check-equal?
-   (sort (sym-mods 'printf) symbol<?)
-   '(lang/htdp-advanced lazy plai/gc2/mutator plai/mutator racket/base))
-  (check-equal?
-   (sort (sym-mods 'rectangle) symbol<?)
-   '(2htdp/image htdp/image slideshow/pict teachpack/deinprogramm/image)))
+  (check-equal? (sort (sym-mods 'printf) symbol<?)
+                '(lang/htdp-advanced lazy plai/gc2/mutator plai/mutator
+                                     racket racket/base)
+                "this test may not be portable")
+  (check-equal? (sort (sym-mods 'rectangle) symbol<?)
+                '(2htdp/image htdp/image slideshow slideshow/pict
+                              teachpack/deinprogramm/image)
+                "this test may not be portable"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define/contract (doc-uri/sym-in-mod id mod ext-root-uri)
   (symbol? symbol? (or/c #f string?) . -> . (or/c #f string?)) ;or exn
@@ -82,5 +90,5 @@
 
 (module+ test
   (check-equal?
-   (doc-uri/sym-in-mod 'write 'racket "http://docs.racket-lang.org/")
-   "http://docs.racket-lang.org/reference/Writing.html#(def._((quote._~23~25kernel)._write))"))
+   (doc-uri/sym-in-mod 'write 'racket "/")
+   "/reference/Writing.html#(def._((quote._~23~25kernel)._write))"))
