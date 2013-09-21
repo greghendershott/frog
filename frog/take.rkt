086178b5ed8293baf8f5161f6704815d5328b9d6
Greg Hendershott <greghendershott@gmail.com>
2013-09-21 10:46:24 -0400

    Move tests into `test` submodule.

--- frog/take.rkt
+++ frog/take.rkt
@@ -6,6 +6,8 @@
          split-at<=
          take-every)
 
+(module+ test (require rackunit))
+
 ;; Like `take`, but OK if list has fewer than `n` members.
 (define (take<= xs n)
   (for/list ([x (in-list xs)]
@@ -17,19 +19,19 @@
   (cond [(<= n (length xs)) (split-at xs n)]
         [else (values xs '())]))
 
-(require rackunit)
-(let ()
-  (define-values (a b) (split-at<= '(1 2 3 4 5) 2))
-  (check-equal? a '(1 2))
-  (check-equal? b '(3 4 5)))
-(let ()
-  (define-values (a b) (split-at<= '() 1))
-  (check-equal? a '())
-  (check-equal? b '()))
-(let ()
-  (define-values (a b) (split-at<= '(1) 1))
-  (check-equal? a '(1))
-  (check-equal? b '()))
+(module+ test
+  (let ()
+    (define-values (a b) (split-at<= '(1 2 3 4 5) 2))
+    (check-equal? a '(1 2))
+    (check-equal? b '(3 4 5)))
+  (let ()
+    (define-values (a b) (split-at<= '() 1))
+    (check-equal? a '())
+    (check-equal? b '()))
+  (let ()
+    (define-values (a b) (split-at<= '(1) 1))
+    (check-equal? a '(1))
+    (check-equal? b '())))
 
 ;; Run `split-at` over a list, producing a list of lists, each of
 ;; which has `n` elements (except possibly the last one).
@@ -38,9 +40,11 @@
   (cond [(empty? more) (list this)]
         [else (cons this (take-every more n))]))
 
-(check-equal? (take-every '(1 2 3 4 5) 2)
-              '((1 2) (3 4) (5)))
-(check-equal? (take-every '() 1)
-              '(()))
-(check-equal? (take-every '(1) 1)
-              '((1)))
+(module+ test
+  (check-equal? (take-every '(1 2 3 4 5) 2)
+                '((1 2) (3 4) (5)))
+  (check-equal? (take-every '() 1)
+                '(()))
+  (check-equal? (take-every '(1) 1)
+                '((1))))
+
