7164114cf4fd27c10534e88d2f08a49eaf1d6d65
Greg Hendershott <greghendershott@gmail.com>
2013-09-21 10:48:52 -0400

    Rename and reorg.

--- frog/watch-dir.rkt
+++ frog/watch-dir.rkt
@@ -1,7 +1,9 @@
 #lang rackjure
 
 (require file/md5)
-(provide watch-directory)
+(provide watch-directory
+         checksum-path
+         checksum-item)
 
 ;; This is a poor person's version of OS-specific mechanisms like
 ;; FindFirstFileChangeNotification on Windows and fsevents on OS X.
@@ -10,36 +12,18 @@
 ;;
 ;; A version of this which _does_ use FFI to access OS-specific
 ;; services would be great.
-
-(define/contract (checksum-item path)
-  (path? . -> . bytes?)
-  (define (path->integer path)
-    (for/fold ([x 0])
-        ([c (in-string (path->string path))])
-      (+ x (char->integer c))))
-  (define (get-time path)
-    ;; Use the modification time. Hashing the actual file contents
-    ;; would be more reliable, but much more expensive, and not
-    ;; necessary for many applications.
-    ;;
-    ;; If we can't even get the modify time (e.g. permission error)
-    ;; then simply hash the path name. (In that case we can tell if
-    ;; something is created or deleted, but not whether it has been
-    ;; modified.)
-    (with-handlers ([exn:fail? (lambda (_) (path->integer path))])
-      (file-or-directory-modify-seconds path)))
-  (~> (get-time path)
-      number->string
-      string->bytes/utf-8
-      (md5 #f)))
-
-(define/contract (checksum-path path types)
-  (path? (listof (or/c 'file 'dir 'link)) . -> . hash?) ;; path? => bytes?
-  (~> (fold-files (lambda (path type h)
-                    (cond [(memq type types)
-                           (hash-set h path (checksum-item path))]
-                          [else h]))
-                  (hash) path)))
+;;
+;; Example ussage:
+;; (require racket/runtime-path)
+;; (define-runtime-path p "/tmp/test")
+;; (define t (watch-directory p
+;;                            '(file)
+;;                            (lambda (p what)
+;;                              (printf "~a: ~a\n" p what))))
+;; (printf "Watching path ~a. Try changing files there.\n" p)
+;; (displayln "Enter QUIT to stop.")
+;; (void (read))
+;; (kill-thread t)
 
 (define/contract (watch-directory path
                                   types
@@ -47,7 +31,7 @@
                                   #:rate [rate 3])
   ((path? 
     (listof (or/c 'file 'dir 'link))
-    (path? (or/c 'new 'delete 'modify) . -> . any))
+    (path? (or/c 'create 'delete 'modify) . -> . any))
    (#:rate (and/c positive? number?))
    . ->* . thread?)
   (thread (thunk
@@ -56,7 +40,7 @@
              (define new (checksum-path path types))
              (for ([(k v) (in-hash new)])
                (cond [(not (hash-has-key? old k))
-                      (on-change k 'new)]))
+                      (on-change k 'create)]))
              (for ([(k v) (in-hash old)])
                (cond [(not (hash-has-key? new k))
                       (on-change k 'delete)]
@@ -64,16 +48,35 @@
                       (on-change k 'modify)]))
              (loop new)))))
 
-;; ;; Example ussage:
-;; (require racket/runtime-path)
-;; (define-runtime-path p "/tmp/test/")
-;; (pretty-print (checksum-path p '(file dir)))
-;; (define t (watch-directory
-;;            p
-;;            '(file)
-;;            (lambda (p what)
-;;              (printf "~a: ~a\n" p what))))
-;; (printf "Watching paths ~a. Try changing files there.\n" (list p))
-;; (displayln "Enter QUIT to stop.")
-;; (void (read))
-;; (kill-thread t)
+(define/contract (checksum-path path types)
+  (path? (listof (or/c 'file 'dir 'link)) . -> . (hash/c path? bytes?))
+  (fold-files (lambda (path type h)
+                (cond [(memq type types)
+                       (hash-set h path (checksum-item path))]
+                      [else h]))
+              (hash)
+              path))
+
+(define/contract (checksum-item path)
+  (path? . -> . bytes?)
+  (~> path
+      path->uid
+      number->string
+      string->bytes/utf-8
+      (md5 #f)))
+
+(define/contract (path->uid path)
+  (path? . -> . integer?)
+  ;; Try to use the modification time. (Hashing the actual file
+  ;; contents would be more reliable, but much more expensive, and
+  ;; unnecessary for many applications.)
+  ;;
+  ;; If we can't get the modify time (e.g. permission error) then
+  ;; simply hash the path name. (In that case we will be able to
+  ;; report something created or deleted, but modified.)
+  (define (path->integer path)
+    (for/fold ([x 0])
+              ([c (in-string (path->string path))])
+      (+ x (char->integer c))))
+  (with-handlers ([exn? (lambda (_) (path->integer path))])
+    (file-or-directory-modify-seconds path)))
