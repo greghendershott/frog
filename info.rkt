7100e923de4a699f4b4134d7044ce432f3ebd53a
Greg Hendershott <greghendershott@gmail.com>
2013-10-08 14:23:40 -0400

    Fix commit e7429c4f: .travis.yml must install deps, after all.

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang setup/infotab
 (define version "0.7")
 (define collection 'multi)
+;; Please duplicate all _direct_ deps in .travis.yml, too.
 (define deps '(("markdown" "0.5")
                "rackjure"
                "find-parent-dir"))
