c7de1ff91a4e136ae94aa7a2cef302d56de9e9cf
Greg Hendershott <greghendershott@gmail.com>
2013-11-11 10:09:33 -0500

    Bump frog to v0.8 and bump markdown req to v0.7
    That way, `raco pkg update frog` will automatically get the newer
    markdown parser, even without --update-deps.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang setup/infotab
-(define version "0.7")
+(define version "0.8")
 (define collection 'multi)
 ;; Please duplicate all _direct_ deps in .travis.yml, too.
-(define deps '(("markdown" "0.5")
+(define deps '(("markdown" "0.7")
                "rackjure"
                "find-parent-dir"))
