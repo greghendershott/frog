f2927ac5b5f956ecd101ec00d02719dc00b24142
Greg Hendershott <greghendershott@gmail.com>
2013-12-20 18:29:28 -0500

    Add missing deps and build-deps. Fixes #65.

--- info.rkt
+++ info.rkt
@@ -1,6 +1,14 @@
 #lang setup/infotab
 (define version "0.11")
 (define collection 'multi)
-(define deps '(("markdown" "0.9")
+(define deps '("base"
+               "html-lib"
+               "racket-index"
+               "scribble-lib"
+               "srfi-lite-lib"
+               "web-server-lib"
+               ("markdown" "0.9")
                "rackjure"
                "find-parent-dir"))
+(define build-deps '("at-exp-lib"
+                     "rackunit-lib"))
