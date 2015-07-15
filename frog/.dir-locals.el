dc939d2e2a0b0179ba9589fbbcedc782fa938aed
Greg Hendershott <greghendershott@gmail.com>
2015-07-15 17:43:18 -0400

    Add .dir-locals.el

---
+++ frog/.dir-locals.el
@@ -0,0 +1,9 @@
+((nil
+  . ((indent-tabs-mode         . nil)
+     (require-final-newline    . t)
+     (show-trailing-whitespace . t)))
+ (makefile-mode
+  . ((indent-tabs-mode . t)))
+ (racket-mode
+  . ((comment-column . 40)
+     (fill-column    . 78))))
