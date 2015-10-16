e5ca6276d279a69427f1891f6ee44b147716def5
Greg Hendershott <greghendershott@gmail.com>
2015-10-16 08:14:23 -0400

    v0.24: Dep racket 6.0 rackjure 0.9

--- info.rkt
+++ info.rkt
@@ -1,14 +1,15 @@
 #lang setup/infotab
-(define version "0.23")
+(define version "0.24")
 (define collection 'multi)
 (define deps '("base"
+               "find-parent-dir"
                "html-lib"
+               ["markdown" "0.20"]
                "racket-index"
                "scribble-lib"
                "srfi-lite-lib"
-               "web-server-lib"
-               ("markdown" "0.20")
-               ("rackjure" "0.7")
-               "find-parent-dir"))
+               ["racket" "6.0"]
+               ["rackjure" "0.9"]
+               "web-server-lib"))
 (define build-deps '("at-exp-lib"
                      "rackunit-lib"))
