e746260c3320ae24bea7e8e2d19aa36afc8e5bca
Tobias Gerdin <tgerdin@gmail.com>
2017-04-21 22:29:35 +0200

    Revert changes to dir-localss

--- frog/.dir-locals.el
+++ frog/.dir-locals.el
@@ -1,14 +1,9 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
 ((nil
-  (indent-tabs-mode)
-  (require-final-newline . t)
-  (show-trailing-whitespace . t))
+  . ((indent-tabs-mode         . nil)
+     (require-final-newline    . t)
+     (show-trailing-whitespace . t)))
  (makefile-mode
-  (indent-tabs-mode . t))
+  . ((indent-tabs-mode . t)))
  (racket-mode
-  (comment-column . 40)
-  (fill-column . 78))
- (scheme-mode
-  (compile-command . "racket frog.rkt")))
+  . ((comment-column . 40)
+     (fill-column    . 78))))
