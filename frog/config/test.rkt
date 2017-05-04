7de96d52dc28b60e2d2ecc0bf7b3db2ace604d3b
Greg Hendershott <greghendershott@gmail.com>
2017-05-04 16:53:08 -0400

    Fix tests for Racket < 6.3
    Use eval on an s-expression and make-base-namespace
    (instead of eval-syntax on syntax and make-base-empty-namespace).

--- frog/config/test.rkt
+++ frog/config/test.rkt
@@ -3,29 +3,34 @@
 (module+ test
   (require rackunit)
 
-  (check-not-exn (λ () (eval-syntax #'(module m frog/config/main
-                                        (define init #f)
-                                        (define enhance-body #f)
-                                        (define clean #f))
-                                    (make-base-empty-namespace))))
+  (module m frog/config/main
+    (define init #f)
+    (define enhance-body #f)
+    (define clean #f))
+
+  (check-not-exn (λ () (eval '(module m frog/config/main
+                               (define init #f)
+                               (define enhance-body #f)
+                               (define clean #f))
+                             (make-base-namespace))))
 
   (check-exn #rx"frog/config: You must define a function named \"init\""
-             (λ () (eval-syntax #'(module m frog/config/main
-                                    #;(define init #f)
-                                    (define enhance-body #f)
-                                    (define clean #f))
-                                (make-base-empty-namespace))))
+             (λ () (eval '(module m frog/config/main
+                           #;(define init #f)
+                           (define enhance-body #f)
+                           (define clean #f))
+                         (make-base-namespace))))
 
   (check-exn #rx"frog/config: You must define a function named \"enhance-body\""
-             (λ () (eval-syntax #'(module m frog/config/main
-                                    (define init #f)
-                                    #;(define enhance-body #f)
-                                    (define clean #f))
-                                (make-base-empty-namespace))))
+             (λ () (eval '(module m frog/config/main
+                           (define init #f)
+                           #;(define enhance-body #f)
+                           (define clean #f))
+                         (make-base-namespace))))
 
   (check-exn #rx"frog/config: You must define a function named \"clean\""
-             (λ () (eval-syntax #'(module m frog/config/main
-                                    (define init #f)
-                                    (define enhance-body #f)
-                                    #;(define clean #f))
-                                (make-base-empty-namespace)))))
+             (λ () (eval '(module m frog/config/main
+                           (define init #f)
+                           (define enhance-body #f)
+                           #;(define clean #f))
+                         (make-base-namespace)))))
