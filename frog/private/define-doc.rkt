1a7c9791355da7df19d8339fb580d3772b315351
Greg Hendershott <greghendershott@gmail.com>
2017-05-15 16:13:03 -0400

    Add #:ex to define-doc
    Expressions to be used as both doc examples and as unit tests.
    
    Note: We can't use scribble/example before Racket 6.4. Instead use
    scribble/eval.

--- frog/private/define-doc.rkt
+++ frog/private/define-doc.rkt
@@ -3,14 +3,17 @@
 (require (for-syntax racket/base
                      syntax/parse)
          racket/contract/base
-         scribble/srcdoc)
+         scribble/srcdoc
+         (for-doc scribble/eval))
 
 (provide define/doc)
 
 ;; Both `proc-doc` and `proc-doc/names` can be verbose and repetitious.
 ;; Provide an alternative with approximately the same signature as `defproc`.
+;; (Caveat: Can only express ->* contracts, not ->i or ->d.)
 ;;
-;; Caveat: Can only express ->* contracts, not ->i or ->d.
+;; Also, provide a way to list expressions that are used both as doc
+;; examples and as unit tests.
 
 (begin-for-syntax
   (define-syntax-class required-argument
@@ -33,7 +36,19 @@
     (pattern [kw:keyword id:id c:expr default:expr]
              #:with decl #'(kw (id default))
              #:with contract #'(kw c)
-             #:with proc-doc #'(id default))))
+             #:with proc-doc #'(id default)))
+
+  (define-splicing-syntax-class example/test
+    #:attributes (example test)
+    (pattern (~seq #:ex [actual:expr expected:expr])
+             #:with example #'actual
+             #:with test (syntax/loc #'actual ;helpful srcloc when tests fail
+                           (check-equal? actual expected)))
+    ;; This pattern is for example expressions like (require foo) that
+    ;; aren't also a unit test.
+    (pattern (~seq #:ex [actual:expr])
+             #:with example #'actual
+             #:with test #'(begin))))
 
 (define-syntax (define/doc stx)
   (syntax-parse stx
@@ -41,16 +56,23 @@
                opt:optional-argument ...
                result-contract:expr)
         (doc-expr:expr ...)
+        et:example/test ...
         body:expr ...+)
      (with-syntax ([((req-decl     ...) ...) #'(req.decl     ...)]
                    [((opt-decl     ...) ...) #'(opt.decl     ...)]
                    [((req-contract ...) ...) #'(req.contract ...)]
-                   [((opt-contract ...) ...) #'(opt.contract ...)])
+                   [((opt-contract ...) ...) #'(opt.contract ...)]
+                   [(doc-examples ...) (syntax-parse #'(et.example ...)
+                                         [()            #'()] ;avoid (examples)
+                                         [(e:expr ...+) #'((examples e ...))])])
        (syntax/loc stx
          (begin
            (define (id req-decl ... ...
                        opt-decl ... ...)
              body ...)
+           (module+ test
+             (require rackunit)
+             et.test ...)
            (provide
             (proc-doc/names id
                             (->* (req-contract ... ...)
@@ -58,4 +80,4 @@
                                  result-contract)
                             ((req.proc-doc ...)
                              (opt.proc-doc ...))
-                            (doc-expr ...))))))]))
+                            (doc-expr ... doc-examples ...))))))]))
