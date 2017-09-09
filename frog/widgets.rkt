54574c36408f25173a2c64d5f97f1eeb5ce24770
Royall Spence <royall@royall.us>
2017-09-09 14:50:50 -0400

    Add Piwik JS widget
    Fixes #42

--- frog/widgets.rkt
+++ frog/widgets.rkt
@@ -279,3 +279,25 @@
                 src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
         </script>
        })
+
+ (define/doc (piwik [site string?]
+                    [domain string?]
+                    list?)
+   @{Add Piwik tracking code to your template.}
+   @list{
+         <!-- Piwik -->
+         <script type="text/javascript">
+         var _paq = _paq || [];
+         /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+         _paq.push(['trackPageView']);
+         _paq.push(['enableLinkTracking']);
+         (function() {
+             var u="//@|domain|/";
+             _paq.push(['setTrackerUrl', u+'piwik.php']);
+             _paq.push(['setSiteId', '@|site|']);
+             var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+             g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+         })();
+         </script>
+         <!-- End Piwik Code -->
+         })
